9409127e76e0a80317cdfa941170288b15397ed5
talitagroberto <talitagroberto@gmail.com>
2026-09-14 19:30:59 -0300

    fix: add MySQL driver to compile classpath

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ application {
 dependencies {
     implementation("org.liquibase:liquibase-core:4.29.1")
 
-    runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
+    implementation("com.mysql:mysql-connector-j:8.0.33")
 
     compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
