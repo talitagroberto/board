ed30e5f2c0d9be1c0b21a486b5e566c1115c035a
talitagroberto <talitagroberto@gmail.com>
2026-09-14 16:13:42 -0300

    build: modernize Gradle and project configuration

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,20 +1,43 @@
+import org.gradle.api.tasks.compile.JavaCompile
+
 plugins {
-    id("java")
+    java
+    application
 }
 
 group = "br.com.dio"
-version = "1.0-SNAPSHOT"
+version = "1.0.0"
 
 repositories {
     mavenCentral()
 }
 
+java {
+    toolchain {
+        languageVersion.set(JavaLanguageVersion.of(21))
+    }
+}
+
+application {
+    mainClass.set("br.com.dio.Main")
+}
+
 dependencies {
     implementation("org.liquibase:liquibase-core:4.29.1")
-    implementation("mysql:mysql-connector-java:8.0.33")
-    implementation("org.projectlombok:lombok:1.18.34")
 
+    runtimeOnly("com.mysql:mysql-connector-j:8.0.33")
+
+    compileOnly("org.projectlombok:lombok:1.18.34")
     annotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    testCompileOnly("org.projectlombok:lombok:1.18.34")
+    testAnnotationProcessor("org.projectlombok:lombok:1.18.34")
+
+    testImplementation("org.junit.jupiter:junit-jupiter:5.11.0")
+}
+
+tasks.withType<JavaCompile>().configureEach {
+    options.encoding = "UTF-8"
 }
 
 tasks.test {
