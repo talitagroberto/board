3b0506c46e777a6b15395b96db716ff28ec9c6d4
talitagroberto <talitagroberto@gmail.com>
2026-09-14 15:58:26 -0300

    refactor: improve application startup

--- src/main/java/br/com/dio/Main.java
+++ src/main/java/br/com/dio/Main.java
@@ -7,14 +7,27 @@ import java.sql.SQLException;
 
 import static br.com.dio.persistence.config.ConnectionConfig.getConnection;
 
+public final class Main {
 
-public class Main {
+    private Main() {
+    }
 
-    public static void main(String[] args) throws SQLException {
-        try(var connection = getConnection()){
+    public static void main(final String[] args) {
+        try {
+            executeDatabaseMigration();
+            new MainMenu().execute();
+        } catch (SQLException exception) {
+            System.err.println(
+                    "Não foi possível iniciar a aplicação. Verifique a conexão com o banco de dados."
+            );
+            System.err.println("Detalhes: " + exception.getMessage());
+        }
+    }
+
+    private static void executeDatabaseMigration() throws SQLException {
+        try (var connection = getConnection()) {
             new MigrationStrategy(connection).executeMigration();
         }
-        new MainMenu().execute();
     }
 
 }
